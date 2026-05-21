d193d2aeba262f4e1d1289d2f4e0bdd19f9fa7b2
sujebe0508-create <sujebe0508@hufs.ac.kr>
2026-05-21 15:09:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -323,4 +323,4 @@ with tab3:
 # ----------------- 하단 푸터 -----------------
 st.write("")
 st.write("")
-st.caption("© 2026 Defy.ai - 숭실대학교 IT대학 X 경제학과 행동교정 융합 프로젝트 (Simulated)")
+st.caption("© 2026 Defy.ai - 한국외대 기계학습 프로젝트 (Simulated)")
